f52c7172fbbd7ab4b6cf6a47c9af38435a47b32e
AleksandrPodarkin <alexchembarov@gmail.com>
2023-07-22 23:41:35 +0400

    home_work

--- home_work/home_work_6/task_1/task1.py
+++ home_work/home_work_6/task_1/task1.py
@@ -3,9 +3,3 @@ from check import check_data
 check_data("12.06.1995")
 check_data("122.06.1995")
 check_data("1.01.2024")
-
-# PS C:\Users\Bekinsale\Desktop\project_py_charm> python .\home_work\home_work_6\task_1\check.py 21.05.1777
-# 21.05.1777
-# не високосный год
-# True
-# PS C:\Users\Bekinsale\Desktop\project_py_charm>
