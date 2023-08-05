7ccfaa703a6940e23ea7c43cbd228637c703c676
AleksandrPodarkin <alexchembarov@gmail.com>
2023-08-06 00:22:27 +0400

    home_work

--- home_work/home_work_10/cash_machine_module.py
+++ home_work/home_work_10/cash_machine_module.py
@@ -3,7 +3,7 @@ class CashMachine:
     __COMMISSION_BANK = 0.9
     __DIVISION_CHECK = 50
     __MIN_COMMISSION = 30
-    __COMMISSION = 60
+    __COMMISSION = 1.5
     __MAX_COMMISSION = 600
     __BONUS_OPERATION = 3
     __BONUS_PERCENT = 1.03
@@ -32,12 +32,10 @@ class CashMachine:
         add = float(input("внесите сумму кратную 50: "))
         if add % self.__DIVISION_CHECK == 0:
             self.__cash += add
-            self.__counter += 1
             self.__history.append(f"пополнение счета на {str(add)} у.е, баланс: {round(self.__cash, 2)} у.е")
         else:
             print("! ошибка внесения денег: неверная сумма")
             self.__history.append(f"ошибка внесения денег: неверная сумма, баланс: {round(self.__cash, 2)} у.е")
-            self.__counter += 1
 
     def __give_money(self):
         """Метод для снятия денег"""
@@ -48,28 +46,22 @@ class CashMachine:
                 percent = self.__MIN_COMMISSION
             if percent > self.__MAX_COMMISSION:
                 percent = self.__MAX_COMMISSION
-
             if self.__cash < (take + percent):
                 print(f"! ошибка снятия денег: недостаточно средств баланс: {round(self.__cash, 2)} у.е")
-                self.__history.append(f"ошибка снятия денег: недостаточно "
-                                      f"средств, баланс: {round(self.__cash, 2)} у.е")
-                self.__counter += 1
+                self.__history.append(f"ошибка снятия денег: недостаточно средств, баланс: {round(self.__cash, 2)} у.е")
             else:
                 self.__cash -= (take + percent)
-                self.__counter += 1
                 self.__history.append(f"снятие {str(take)} у.е, "
                                       f"баланс: {round(self.__cash, 2)} у.е (комиссия банка {percent} у.е)")
         else:
             print("ошибка снятия денег! Неверная сумма")
             self.__history.append(f"ошибка снятия денег: неверная сумма баланс: {round(self.__cash, 2)} у.е")
-            self.__counter += 1
 
     def __print_history(self):
         """Метод для печати истории операций"""
         print("\nистория операций: ")
         print(" \n".join(self.__history))
-        input("для продолжения нажмите любую клавишу...")
-        self.__counter += 1
+        input("для продолжения нажмите ENTER...")
 
     def __give_percent(self):
         """Метод начисления процентов за каждую третью операцию в банкомате"""
@@ -82,6 +74,7 @@ class CashMachine:
     def work(self):
         """Метод банкомат cash_machine !"""
         while True:
+            self.__counter += 1
             if self.__cash > self.__FREE_LIMIT:
                 self.__cash *= self.__COMMISSION_BANK
                 print(f'комиссия банка 10% баланс: {round(self.__cash, 2)} у.е')
