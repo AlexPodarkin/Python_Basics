0257733ea5e055d403dc1f73ce8fdd2e738461b0
AleksandrPodarkin <alexchembarov@gmail.com>
2023-08-22 13:31:14 +0400

    home_work

--- home_work/home_work_15/controller_.py
+++ home_work/home_work_15/controller_.py
@@ -1,31 +1,29 @@
 import model_
+import view_
 
-__model = model_.ModelCashMachine()
-
-
-def controller_launch():
-    return __model.launch_cash_machine()
-
-
-def controller_put_money(add):
-    return __model.put_money(add)
-
-
-def controller_give_money(take):
-    return __model.give_money(take)
-
-
-def controller_print_history():
-    return __model.print_history()
-
-
-def controller_save_json():
-    return __model.save_json()
-
-
-def controller_load_json():
-    return __model.load_json()
-
-
-def controller_give_percent():
-    return __model.give_percent()
+if __name__ == '__main__':
+    __model = model_.ModelCashMachine()
+    while True:
+        view_.print_menu()
+        view_.view_print(__model.launch_cash_machine())
+        match view_.view_chose('ваш выбор -> '):
+            case "1":
+                try:
+                    view_.view_print(__model.put_money(float(view_.view_chose('внесите сумму кратную 50: '))))
+                except ValueError:
+                    view_.view_chose('Ошибка ввода (воспользуйтесь цифровой клавиатурой) !')
+            case "2":
+                try:
+                    view_.view_print(__model.give_money(float(view_.view_chose('внесите сумму кратную 50: '))))
+                except ValueError:
+                    view_.view_chose('Ошибка ввода (воспользуйтесь цифровой клавиатурой) !')
+            case "3":
+                view_.view_print(__model.print_history())
+                view_.wait()
+            case "4":
+                view_.view_print(__model.save_json())
+            case "5":
+                view_.view_print(__model.load_json())
+            case "0":
+                quit()
+        view_.view_print(__model.give_percent())
--- home_work/home_work_15/view_.py
+++ home_work/home_work_15/view_.py
@@ -1,53 +1,23 @@
-import controller_
+def print_menu():
+    """Функция печати меню банкомата"""
+    print(f'      МЕНЮ:\n'
+          f'1. Чтобы пополнить\n'
+          f'2. Чтобы снять\n'
+          f'3. Для печати истории операций\n'
+          f'4. Сохранить состояние в JSON\n'
+          f'5. Загрузить состояние из JSON \n'
+          f'0. Выход')
 
 
-if __name__ == '__main__':
-    def print_menu():
-        """Функция печати меню банкомата"""
-        print(f'      МЕНЮ:\n'
-              f'1. чтобы пополнить\n'
-              f'2. чтобы снять\n'
-              f'3. для печати истории операций\n'
-              f'4. Сохранить состояние в JSON\n'
-              f'5. Загрузить состояние из JSON \n'
-              f'0. Выход')
+def view_print(text: str):
+    """Функция печати"""
+    print(text)
 
 
-    while True:
+def view_chose(text):
+    """Функция ввода"""
+    return input(text)
 
-        msg = controller_.controller_launch()
-        print_menu()
-        print(f'{msg}')
-        action = input("ваш выбор -> ")
-        match action:
-            case "1":
-                try:
-                    add = float(input('внесите сумму кратную 50: '))
-                    msg = controller_.controller_put_money(add)
-                    print(msg)
-                except ValueError:
-                    print('Неверный ввод !')
-                    continue
 
-            case "2":
-                try:
-                    take = float(input('внесите сумму кратную 50: '))
-                    msg = controller_.controller_give_money(take)
-                    print(msg)
-                except ValueError:
-                    print('Неверный ввод !')
-                    continue
-            case "3":
-                msg = controller_.controller_print_history()
-                print(msg)
-                input("для продолжения нажмите ENTER...")
-            case "4":
-                msg = controller_.controller_save_json()
-                print(msg)
-            case "5":
-                msg = controller_.controller_load_json()
-                print(msg)
-            case "0":
-                quit()
-        msg = controller_.controller_give_percent()
-        print(msg)
+def wait():
+    input("для продолжения нажмите ENTER...")
